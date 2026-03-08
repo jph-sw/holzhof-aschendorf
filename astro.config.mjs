f7558c260186395661bb07fe40eb6de9574805f8
jph-sw <jph.sw@proton.me>
2026-03-08 14:23:12 +0100

    feat: set base-url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jph-sw.github.io",
+  base: "/holzhof-aschendorf",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
